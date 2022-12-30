8dc3c4b24febd1caad1c66d997983bda37644ea4
Emma <ironicallyshort737@hotmail.com>
2022-12-29 23:41:41 -0700

    included marker font, changed channel list style

--- dash/src/components/channelList/index.tsx
+++ dash/src/components/channelList/index.tsx
@@ -27,7 +27,7 @@ export const ChannelList: FunctionComponent = () => {
   }, [fetchChannelList])
 
   return (
-    <div class="dark:bg-grizzly-100/70 bg-grizzly-100/10 h-full rounded-lg p-2">
+    <div class="h-full p-2">
       <div class="flex justify-between">
         <h3 class="uppercase font-bold mt-2">channels</h3>
         <button class="bg-green">Create Channel</button>
@@ -35,7 +35,7 @@ export const ChannelList: FunctionComponent = () => {
       <hr class="border-purple-50 my-4" />
       <ul class="flex flex-col gap-2">
         {channels.map(c => (
-          <li class="flex justify-between p-4 hover:bg-white/20 bg-black/10 rounded-md cursor-pointer transition-bg duration-300">
+          <li class="etched flex justify-between p-4 hover:bg-white/20 bg-black/10 rounded-md cursor-pointer transition-bg duration-300">
             <span>{c.id}</span>
             <span>{c.listeners}</span>
           </li>
